0b4e48aa5b3f1bb3a31ea120a2a82fb9f67eb249
daniel <danielo.visage@gmail.com>
2021-04-25 13:38:26 +0200

    remove default styles, content, set up sass, typescript, empty components

---
+++ src/components/MovieList.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import MovieCard from './MovieCard';
+
+function MovieList() {
+  return (
+    <div>
+      <h1>Movie List</h1>
+      <MovieCard />
+    </div>
+  )
+}
+
+export default MovieList
